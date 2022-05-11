8b2b2214e065c6dbb5194e65fcdd0e4bcc05da04
ihossen016 <ihossen016@gmail.com>
2022-05-11 20:26:21 +0600

    firebase added

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/firestre";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDy18kL6py7TYjD1UJIeQLibJqPNAedG2o",
+  authDomain: "trackmoney-351ea.firebaseapp.com",
+  projectId: "trackmoney-351ea",
+  storageBucket: "trackmoney-351ea.appspot.com",
+  messagingSenderId: "363515036090",
+  appId: "1:363515036090:web:733e3ec68fe7a988579d83",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const projectFirestore = firebase.firestore();
+
+export { projectFirestore };
