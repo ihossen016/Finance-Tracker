95c60728edd83baff975d3ae2779317aa12e7f70
ihossen016 <ihossen016@gmail.com>
2022-05-12 22:41:21 +0600

    firestore hook created

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -8,6 +8,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.content}>transaction list</div>
+
       <div className={styles.sidebar}>
         <TransactionForm />
       </div>
