4a967f3a0201802f1f331bc1d8201712774ef5cd
ihossen016 <ihossen016@gmail.com>
2022-05-13 00:56:56 +0600

    query added to fetch unique user data

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,12 +1,20 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { projectFirestore } from "../firebase/config";
 
-export const useCollection = collection => {
+export const useCollection = (collection, _query) => {
   const [documents, setDocuments] = useState(null);
   const [error, setError] = useState(null);
 
+  // if we don't use a ref --> infinite loop in useEffect
+  // _query is an array and is "different" on every function call
+  const query = useRef(_query).current;
+
   useEffect(() => {
     let ref = projectFirestore.collection(collection);
+
+    if (query) {
+      ref = ref.where(...query);
+    }
 
     const unsubscribe = ref.onSnapshot(
       snapshot => {
@@ -27,7 +35,7 @@ export const useCollection = collection => {
 
     // unsubscribe on unmount / cleanup function
     return () => unsubscribe();
-  }, [collection]);
+  }, [collection, query]);
 
   return { documents, error };
 };
